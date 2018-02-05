6150fc4241e6459f63d9419c0424e6c50e769805
S4cha <sachadso@gmail.com>
2018-02-05 17:59:10 +0100

    Fixes bug with Library receiving updates after view hidden

--- Source/Library/YPLibraryVC.swift
+++ Source/Library/YPLibraryVC.swift
@@ -72,6 +72,7 @@ public class YPLibraryVC: UIViewController, PermissionCheckable {
         super.viewWillDisappear(animated)
         pausePlayer()
         NotificationCenter.default.removeObserver(self)
+        PHPhotoLibrary.shared().unregisterChangeObserver(self)
     }
     
     // MARK: - Crop control
