afbdb3162176f37f68ea53bda8725d8292f6b6c8
S4cha <sachadso@gmail.com>
2018-05-14 15:10:38 +0200

    Use default back button (`shouldChangeDefaultBackButtonIcon = false`)

--- Source/Configuration/YPIcons.swift
+++ Source/Configuration/YPIcons.swift
@@ -10,7 +10,7 @@ import UIKit
 
 public struct YPIcons {
 
-    public var shouldChangeDefaultBackButtonIcon = true
+    public var shouldChangeDefaultBackButtonIcon = false
     public var hideBackButtonTitle = true
     
     public var backButtonIcon: UIImage = imageFromBundle("yp_arrow_left")
