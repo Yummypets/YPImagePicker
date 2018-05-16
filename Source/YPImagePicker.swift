804b25e5c5ac943eb177c315e5db6eeb9f0fbb7e
S4cha <sachadso@gmail.com>
2018-05-16 15:58:30 +0200

    Use black tintColor by default when none is set

--- Source/YPImagePicker.swift
+++ Source/YPImagePicker.swift
@@ -143,6 +143,11 @@ public class YPImagePicker: UINavigationController {
                 }
             }
         }
+        
+        // If user has not customized the Nav Bar tintColor, then use black.
+        if UINavigationBar.appearance().tintColor == nil  {
+            UINavigationBar.appearance().tintColor  = .black
+        }
     }
     
     deinit {
