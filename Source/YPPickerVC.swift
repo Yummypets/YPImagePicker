0a5626b40f1d17a865da97869f9a6261b4bc32eb
pepsin <pepsin@me.com>
2019-09-16 23:24:47 +0800

    Hides cancel button on demand

--- Source/YPPickerVC.swift
+++ Source/YPPickerVC.swift
@@ -263,11 +263,13 @@ open class YPPickerVC: YPBottomPager, YPBottomPagerDelegate {
     }
     
     func updateUI() {
-        // Update Nav Bar state.
-        navigationItem.leftBarButtonItem = UIBarButtonItem(title: YPConfig.wordings.cancel,
-                                                           style: .plain,
-                                                           target: self,
-                                                           action: #selector(close))
+		if !YPConfig.hidesCancelButton {
+	        // Update Nav Bar state.
+	        navigationItem.leftBarButtonItem = UIBarButtonItem(title: YPConfig.wordings.cancel,
+	                                                           style: .plain,
+	                                                           target: self,
+	                                                           action: #selector(close))
+		}
         
         switch mode {
         case .library:
