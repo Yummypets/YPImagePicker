129a41ebe6169b88e0af3b7275b486487f4ba645
S4cha <sachadso@gmail.com>
2019-10-02 10:34:26 +0200

    Force modalPresentationStyle fullScreen

--- Source/YPImagePicker.swift
+++ Source/YPImagePicker.swift
@@ -46,6 +46,7 @@ open class YPImagePicker: UINavigationController {
         YPImagePickerConfiguration.shared = configuration
         picker = YPPickerVC()
         super.init(nibName: nil, bundle: nil)
+        modalPresentationStyle = .fullScreen // Force .fullScreen as iOS 13 now shows modals as cards by default.
         picker.imagePickerDelegate = self
     }
     
