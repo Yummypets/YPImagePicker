6839864da946f9baa73fc1de5d4cdd9035ab5ab4
NikKovIos <nikkovios@gmail.com>
2019-10-12 23:09:40 +0300

    Fixed cancel button color.

--- Source/YPPickerVC.swift
+++ Source/YPPickerVC.swift
@@ -268,7 +268,8 @@ open class YPPickerVC: YPBottomPager, YPBottomPagerDelegate {
                                                            style: .plain,
                                                            target: self,
                                                            action: #selector(close))
-        
+        navigationItem.leftBarButtonItem?.tintColor = YPConfig.colors.tintColor
+
         switch mode {
         case .library:
             setTitleViewWithTitle(aTitle: libraryVC?.title ?? "")
