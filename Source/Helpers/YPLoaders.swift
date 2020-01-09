bf68438886ba1a6ca436ef6baa3fecff3feedcf3
S4cha <sachadso@gmail.com>
2020-01-09 11:49:41 +0100

    Fixes Library Loader in dark mode

--- Source/Helpers/YPLoaders.swift
+++ Source/Helpers/YPLoaders.swift
@@ -12,9 +12,17 @@ struct YPLoaders {
 
     static var defaultLoader: UIBarButtonItem {
         let spinner = UIActivityIndicatorView(style: .gray)
-        spinner.color = YPConfig.colors.navigationBarActivityIndicatorColor
+        if let spinnerColor = YPConfig.colors.navigationBarActivityIndicatorColor {
+            spinner.color = spinnerColor
+        } else {
+            if #available(iOS 13, *) {
+                let spinnerColor = UIColor { trait -> UIColor in
+                    return trait.userInterfaceStyle == .dark ? .white : .gray
+                }
+                spinner.color = spinnerColor
+            }
+        }
         spinner.startAnimating()
         return UIBarButtonItem(customView: spinner)
     }
-    
 }
