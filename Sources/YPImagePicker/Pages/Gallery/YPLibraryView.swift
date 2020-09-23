2cf2d150bb0861f2079bc44b56c17aabf5e5d5aa
s4cha <sachadso@gmail.com>
2020-09-23 11:41:07 +0200

    Fixing resources linking inside package

--- Sources/YPImagePicker/Pages/Gallery/YPLibraryView.swift
+++ Sources/YPImagePicker/Pages/Gallery/YPLibraryView.swift
@@ -90,10 +90,8 @@ final class YPLibraryView: UIView {
 extension YPLibraryView {
     
     class func xibView() -> YPLibraryView? {
-        let bundle = Bundle(for: YPPickerVC.self)
-        let nib = UINib(nibName: "YPLibraryView", bundle: bundle)
-        let xibView = nib.instantiate(withOwner: self, options: nil)[0] as? YPLibraryView
-        return xibView
+        let nib = UINib(nibName: "YPLibraryView", bundle: .module)
+        return nib.instantiate(withOwner: self, options: nil).first as? YPLibraryView
     }
     
     // MARK: - Overlay view
