2b1125bbacab3187c0a1bbb21de4881a6df81bef
sachadso@gmail.com <sachadso@gmail.com>
2021-08-22 23:36:03 +0200

    Fixing build (missing/moved fil refs/ missing imports)

--- Source/Filters/YPFilterCollectionViewCell.swift
+++ Source/Filters/YPFilterCollectionViewCell.swift
@@ -7,6 +7,7 @@
 //
 
 import Stevia
+import UIKit
 
 class YPFilterCollectionViewCell: UICollectionViewCell {
     
--- Source/Models/YPFlashMode.swift
+++ Source/Models/YPFlashMode.swift
@@ -6,6 +6,7 @@
 //
 
 import AVFoundation
+import UIKit
 
 enum YPFlashMode {
     case off
