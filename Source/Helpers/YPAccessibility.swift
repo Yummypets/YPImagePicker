6c445ac6ef36e4416e762f95ac8dd935e952187f
Hari Prasad <hhariprsd@gmail.com>
2021-10-12 16:29:23 -0400

    Adding a Protocol to set Accessibility information

---
+++ Source/Helpers/YPAccessibility.swift
@@ -0,0 +1,12 @@
+//
+//  YPAccessibility.swift
+//  YPImagePicker
+//
+//  Created by Hari Prasad on 10/12/21.
+//
+
+import Foundation
+
+protocol Accessibility: NSObject {
+    func setAccessibilityInfo()
+}
