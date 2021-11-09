f31ba60b8b86647630942011e2bf752e27064cb1
Rehan Ali <rehannali1@outlook.com>
2021-11-09 19:47:11 +0500

    Update YPCameraVC.swift
    Fix Photos permission while taking picture

--- Sources/YPImagePicker/Code/Pages/Photo/YPCameraVC.swift
+++ Sources/YPImagePicker/Code/Pages/Photo/YPCameraVC.swift
@@ -64,17 +64,19 @@ internal final class YPCameraVC: UIViewController, UIGestureRecognizerDelegate, 
     }
     
     func start() {
-        doAfterCameraPermissionCheck { [weak self] in
-            guard let previewContainer = self?.v.previewViewContainer else {
-                return
-            }
-
-            self?.photoCapture.start(with: previewContainer, completion: {
-                DispatchQueue.main.async {
-                    self?.isInited = true
-                    self?.updateFlashButtonUI()
+        doAfterLibraryPermissionCheck { [weak self] in
+            self?.doAfterCameraPermissionCheck { [weak self] in
+                guard let previewContainer = self?.v.previewViewContainer else {
+                    return
                 }
-            })
+                
+                self?.photoCapture.start(with: previewContainer, completion: {
+                    DispatchQueue.main.async {
+                        self?.isInited = true
+                        self?.updateFlashButtonUI()
+                    }
+                })
+            }
         }
     }
 
