feb14234e5c73f0c1142959d9d53f806ec3281a9
zcohen <zeph.cohen@rewardstyle.com>
2024-01-29 13:37:15 -0500

    chore: apply pr suggestion

--- Source/Helpers/Extensions/CMTime+Extensions.swift
+++ Source/Helpers/Extensions/CMTime+Extensions.swift
@@ -10,11 +10,11 @@ import AVFoundation
 import Foundation
 
 extension CMTime {
-    var durationText:String {
+    var durationText: String {
         let totalSeconds = Int(CMTimeGetSeconds(self))
-        let hours:Int = Int(totalSeconds / 3600)
-        let minutes:Int = Int(totalSeconds % 3600 / 60)
-        let seconds:Int = Int((totalSeconds % 3600) % 60)
+        let hours: Int = Int(totalSeconds / 3600)
+        let minutes: Int = Int(totalSeconds % 3600 / 60)
+        let seconds: Int = Int((totalSeconds % 3600) % 60)
 
         if hours > 0 {
             return String(format: "%i:%02i:%02i", hours, minutes, seconds)
