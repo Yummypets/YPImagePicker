d9446d6982ea9f0a4b98442f24600e00d6a3b880
zcohen <zeph.cohen@rewardstyle.com>
2024-02-29 11:02:51 -0500

    fix: [CC-917] check max trim duration
    also checks for the minimum of the trimmer max duration and total seconds when determining to render timestamp dots in 5 secondd intervals or not.

--- Source/Filters/Trim/YPTimeStampScrollView.swift
+++ Source/Filters/Trim/YPTimeStampScrollView.swift
@@ -32,6 +32,7 @@ class YPTimeStampScrollableView: UIScrollView {
     var timeBarLargeCircleColor: UIColor?
     var timeBarSmallCircleColor: UIColor?
     var timeStampFont: UIFont?
+    var trimmerMaxDuration: Double = 0
 
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -80,11 +81,16 @@ class YPTimeStampScrollableView: UIScrollView {
         if totalSeconds < 30 {
             // Clips less then 30 seconds will have a scale of 1 dot = 1 second and every 5th dot will render a timestamp label.
             timeStamps = generateTimeStampViewModels(amount: Int(totalSeconds), timeStampInterval: 5, duration: 1)
-        } else {
+        } else if totalSeconds >= 30, totalSeconds <= 61 || min(trimmerMaxDuration, totalSeconds) == 60.0 {
             let fiveSecondDivisor: Double = 5.0
             timeRangeAmount = round(totalSeconds / fiveSecondDivisor)
-            // Clips over 30 seconds will have a scale of 1 dot = 5 seconds and every 3rd dot will render a timestamp label.
+            // Clips between 30 seconds and 61 seconds or if the trimmer max duration is 1 minute will have a scale of 1 dot = 5 seconds and every 3rd dot will render a timestamp label.
             timeStamps = generateTimeStampViewModels(amount: Int(timeRangeAmount), timeStampInterval: 3, duration: 5)
+        } else {
+            let tenSecondDivisor: Double = 10
+            timeRangeAmount = round(totalSeconds / tenSecondDivisor)
+            // Clips greater then 61 seconds will have a scale of 1 dot = 10 seconds and every 3rd dot will render a timestamp label.
+            timeStamps = generateTimeStampViewModels(amount: Int(timeRangeAmount), timeStampInterval: 3, duration: 10)
         }
         return timeStamps
     }
