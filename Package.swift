d5836f5a764d9043c36f04c27b36d8de97e7a310
Luke Martin-Resnick <63889477+lmartinresnick@users.noreply.github.com>
2025-06-19 17:55:27 -0700

    chore: update stevia package to 6.2.0 for iOS 26 support

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/freshOS/Stevia",
-            .exact("5.1.2")
+            .upToNextMajor(from: "6.2.0")
         ),
         .package(
             url: "https://github.com/rewardStyle/PryntTrimmerView",
