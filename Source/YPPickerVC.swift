73caa448001647f30261b48de7d8ee221042848a
Dipika <dipika.ios.bt@gmail.com>
2025-10-29 13:46:11 +0530

    Added close button on image picker screen

--- Source/YPPickerVC.swift
+++ Source/YPPickerVC.swift
@@ -296,8 +296,10 @@ open class YPPickerVC: YPBottomPager, YPBottomPagerDelegate {
     
     func updateUI() {
         if !YPConfig.hidesCancelButton {
-            // Update Nav Bar state.
-            navigationItem.leftBarButtonItem = UIBarButtonItem(customView: setSelectMultipleView())
+            navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(named: "ic_cancel"),
+                                                               style: .plain,
+                                                               target: self,
+                                                               action: #selector(close))
         }
         switch mode {
         case .library:
